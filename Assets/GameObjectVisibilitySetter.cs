851898c573bd6b36b8bbae5b7857e3ae86f8c6bb
phutaekwondo <phutaekwondo2001@gmail.com>
2022-12-18 16:19:51 +0700

    table colider

---
+++ Assets/GameObjectVisibilitySetter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectVisibilitySetter : MonoBehaviour
+{
+    public List<GameObject> m_hiddenObjects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideObjectsInHiddenObjectsList();
+    }
+
+    void HideObjectsInHiddenObjectsList()
+    {
+        // disable MeshRenderer component on all objects in the list and their child objects, recursively
+        foreach (GameObject obj in m_hiddenObjects)
+        {
+            MeshRenderer[] renderers = obj.GetComponentsInChildren<MeshRenderer>();
+            foreach (MeshRenderer renderer in renderers)
+            {
+                renderer.enabled = false;
+            }
+        }
+    }
+}
