9ae1793ca82d660b15b9009b9aff07f7fb878456
phutaekwondo <phutaekwondo2001@gmail.com>
2022-12-19 23:32:25 +0700

    idk

--- Assets/Scripts/ShootController.cs
+++ Assets/Scripts/ShootController.cs
@@ -12,7 +12,8 @@ public class ShootController : MonoBehaviour
     public GameObject m_rack; // drag and drop the Rack in the inspector
     public LineRenderer m_aimLineRenderer; // drag and drop the LineRenderer in the inspector
 
-    public bool m_isEnabled = true;
+    private bool m_isEnabled = true;
+    private bool m_isAbleToShoot = true;
 
     public float m_maxPower = 1.0f;
     public float m_powerIncreaseRate = 0.2f;
@@ -34,9 +35,19 @@ public class ShootController : MonoBehaviour
         m_isBallsMoving = IsBallsMoving_Calculate();
 
         // handle state input and update state
-        m_state = m_state.HandleInput();
+        m_state = m_state.Update();
         // handle input
         HandleInput();
+
+        // update aim visibility depend on the ability to shoot
+        if ( m_isAbleToShoot )
+        {
+            ShowAim();
+        }
+        else
+        {
+            HideAim();
+        }
 
         // update the powerbar mask
         SetPowerbarMask( m_power / m_maxPower );
@@ -53,10 +64,24 @@ public class ShootController : MonoBehaviour
         }
     }
 
+    public bool IsAbleToShoot()
+    {
+        return m_isAbleToShoot;
+    }
+
+    public void SetAbleToShoot( bool isAbleToShoot )
+    {
+        m_isAbleToShoot = isAbleToShoot;
+    }
+
+    public bool IsEnable()
+    {
+        return m_isEnabled;
+    }
+
     public void Enable() 
     {
         m_isEnabled = true;
-        ShowAim();
     }
 
     public void Disable()
@@ -77,6 +102,8 @@ public class ShootController : MonoBehaviour
 
     public void IncreasePower()
     {
+        if ( !m_isAbleToShoot ) return;
+
         m_power += m_powerIncreaseRate * m_maxPower * Time.deltaTime;
         if ( m_power > m_maxPower ) m_power = m_maxPower;
     }
@@ -115,6 +142,8 @@ public class ShootController : MonoBehaviour
 
     private void Shoot()
     {
+        if ( !m_isAbleToShoot ) return;
+
         //get the vector from the aim point to the ball
         Vector3 direction = m_aimPoint.transform.position - m_cueBall.transform.position;
         //normalize the vector
@@ -142,22 +171,30 @@ public class ShootController : MonoBehaviour
             m_shootController = shootController;
         }
 
-        public abstract ShootControllerState HandleInput();
+        public abstract ShootControllerState Update();
     }
 
     class ShootControllerState_Static : ShootControllerState
     {
         public ShootControllerState_Static(ShootController shootController) : base(shootController)
         {
-            m_shootController.ShowAim();
+            m_shootController.SetAbleToShoot(true);
         }
 
-        public override ShootControllerState HandleInput()
+        public override ShootControllerState Update()
         {
+            //hanlde input
             if (Input.GetMouseButtonDown(0))
             {
                 return new ShootControllerState_IncreasePower(m_shootController);
             }
+
+            // if balls are moving, go to the balls moving state
+            if (m_shootController.IsBallsMoving())
+            {
+                return new ShootControllerState_BallsMoving(m_shootController);
+            }
+
             return this;
         }
     }
@@ -166,11 +203,12 @@ public class ShootController : MonoBehaviour
     {
         public ShootControllerState_IncreasePower(ShootController shootController) : base(shootController)
         {
-            m_shootController.ShowAim();
+            m_shootController.SetAbleToShoot(true);
         }
 
-        public override ShootControllerState HandleInput()
+        public override ShootControllerState Update()
         {
+            //hanlde input
             if (Input.GetMouseButton(0))
             {
                 m_shootController.IncreasePower();
@@ -182,6 +220,13 @@ public class ShootController : MonoBehaviour
                 m_shootController.ResetPower();
                 return new ShootControllerState_BallsMoving(m_shootController);
             }
+
+            //if the balls are moving, go to the balls moving state
+            if (m_shootController.IsBallsMoving())
+            {
+                return new ShootControllerState_BallsMoving(m_shootController);
+            }
+
             return this;
         }
     }
@@ -190,10 +235,10 @@ public class ShootController : MonoBehaviour
     {
         public ShootControllerState_BallsMoving(ShootController shootController) : base(shootController)
         {
-            shootController.HideAim();
+            m_shootController.SetAbleToShoot(false);
         }
 
-        public override ShootControllerState HandleInput()
+        public override ShootControllerState Update()
         {
             if (!m_shootController.IsBallsMoving())
             {
