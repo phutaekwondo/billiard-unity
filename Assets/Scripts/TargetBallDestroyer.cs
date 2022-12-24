6f235f77522b23b1a381c0e25251f85787f32517
phutaekwondo <phutaekwondo2001@gmail.com>
2022-12-24 14:22:08 +0700

    destroy TargetBall when it hit bottom surface

--- Assets/Scripts/TargetBallDestroyer.cs
+++ Assets/Scripts/TargetBallDestroyer.cs
@@ -2,4 +2,10 @@ using UnityEngine;
 
 public class TargetBallDestroyer : MonoBehaviour
 {
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("TargetBall")) {
+            Destroy(other.gameObject);
+            Debug.Log("TargetBall Destroyed");
+        }
+    }
 }
