693657940a8cd3ee08a83ebd0f11cdaecb42a886
phutaekwondo <phutaekwondo2001@gmail.com>
2023-03-05 20:51:47 +0700

    working on rotate the cuestick

--- Assets/Scripts/Player/CueStick.cs
+++ Assets/Scripts/Player/CueStick.cs
@@ -2,8 +2,24 @@ using UnityEngine;
 
 public class CueStick : MonoBehaviour
 {
+    //REFERENCES
+    [SerializeField] private GameObject m_bumper; // the bumper has the same global position as the cue stick
+    [SerializeField] private GameObject m_tip;
+
+    //VIRAIBLES
+    private float m_cueStickLength;
     private const float MIN_AIMING_DISTANCE_FROM_HEAD = 0.5f;
     private const float MAX_AIMING_DISTANCE_FROM_HEAD = 1f;
+    private float m_power = 0f;
+    private Vector3 m_aimingDirection = Vector3.zero;
+    private Vector3 m_aimingPoint = Vector3.zero;
+    private Vector3 DEFAULT_AIMING_DIRECTION = new Vector3(0,0,1);
+
+    //PRIVATE METHODS
+    private void Start() 
+    {
+        m_cueStickLength = Vector3.Distance( m_bumper.transform.position, m_tip.transform.position );
+    }
 
     //PUBLIC METHODS
     public void SetAim(Vector3 direction, Vector3 point)
@@ -26,5 +42,15 @@ public class CueStick : MonoBehaviour
     public void SetVisibility(bool visible)
     {
         GetComponent<MeshRenderer>().enabled = visible;
+    }
+    private void UpdateCueStickTransform()
+    {
+        //position
+        Vector3 tipPosition = m_aimingPoint - m_aimingDirection * (MIN_AIMING_DISTANCE_FROM_HEAD + m_power * (MAX_AIMING_DISTANCE_FROM_HEAD - MIN_AIMING_DISTANCE_FROM_HEAD));
+        Vector3 bumperPosition = tipPosition - m_aimingDirection * m_cueStickLength;
+        transform.position = bumperPosition;
+
+        //rotation
+        //TODO: just rotate y axis
     }
 }
