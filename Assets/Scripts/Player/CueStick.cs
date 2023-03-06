541d61a28910ad71abbb4533a71b761f48f03eb7
phutaekwondo <phutaekwondo2001@gmail.com>
2023-03-06 16:42:58 +0700

    implement UpdateCueStickTransform

--- Assets/Scripts/Player/CueStick.cs
+++ Assets/Scripts/Player/CueStick.cs
@@ -8,7 +8,7 @@ public class CueStick : MonoBehaviour
 
     //VIRAIBLES
     private float m_cueStickLength;
-    [SerializeField] private float MIN_AIMING_DISTANCE_FROM_HEAD = 0.5f;
+    [SerializeField] private float MIN_AIMING_DISTANCE_FROM_HEAD = 0.06f;
     [SerializeField] private float MAX_AIMING_DISTANCE_FROM_HEAD = 1f;
     private float m_power = 0f;
     private Vector3 m_aimingDirection = Vector3.zero;
@@ -16,14 +16,17 @@ public class CueStick : MonoBehaviour
     private Vector3 DEFAULT_AIMING_DIRECTION = new Vector3(0,0,1);
 
     //PRIVATE METHODS
-    private void Start() 
+    private void UpdateCueStickTransform()
     {
-        m_cueStickLength = Vector3.Distance( m_bumper.transform.position, m_tip.transform.position );
-        Debug.Log("CueStick length: " + m_cueStickLength);
-        //test
-        m_aimingDirection = new Vector3(1,0,1);
-        m_aimingPoint = new Vector3(0,0,0);
-        UpdateCueStickTransform();
+        //position
+        Vector3 tipPosition = m_aimingPoint - m_aimingDirection * (MIN_AIMING_DISTANCE_FROM_HEAD + m_power * (MAX_AIMING_DISTANCE_FROM_HEAD - MIN_AIMING_DISTANCE_FROM_HEAD));
+        Vector3 bumperPosition = tipPosition - m_aimingDirection * m_cueStickLength;
+
+        transform.position = new Vector3( bumperPosition.x, transform.position.y, bumperPosition.z );
+
+        //rotation
+        float angle = Vector3.SignedAngle( DEFAULT_AIMING_DIRECTION, m_aimingDirection, Vector3.up );
+        transform.rotation = Quaternion.Euler(0, angle, 0);
     }
 
     //PUBLIC METHODS
@@ -47,18 +50,5 @@ public class CueStick : MonoBehaviour
     public void SetVisibility(bool visible)
     {
         GetComponent<MeshRenderer>().enabled = visible;
-    }
-    private void UpdateCueStickTransform()
-    {
-        //position
-        Vector3 tipPosition = m_aimingPoint - m_aimingDirection * (MIN_AIMING_DISTANCE_FROM_HEAD + m_power * (MAX_AIMING_DISTANCE_FROM_HEAD - MIN_AIMING_DISTANCE_FROM_HEAD));
-        Vector3 bumperPosition = tipPosition - m_aimingDirection * m_cueStickLength;
-        Vector3 middlePosition = (tipPosition + bumperPosition) / 2;
-
-        transform.position = new Vector3( middlePosition.x, transform.position.y, middlePosition.z );
-
-        //rotation
-        //TODO: just rotate y axis
-        transform.LookAt(tipPosition);
     }
 }
