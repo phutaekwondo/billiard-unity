14ee9b34813063b19d91d04ebf2d61d4a658e643
phutaekwondo <phutaekwondo2001@gmail.com>
2023-03-14 16:31:36 +0700

    refernce to imaginationball in Aimer

--- Assets/Scripts/Player/Aimer.cs
+++ Assets/Scripts/Player/Aimer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Aimer : MonoBehaviour
 {
     [SerializeField] GameObject m_cueBall;
+    [SerializeField] GameObject m_imaginationBall;
     public Vector3 GetAimingDirection()
     {
         Vector3 direction = MouseTrackingHelper.GetBallOnTablePositionWithMouse() - m_cueBall.transform.position;
