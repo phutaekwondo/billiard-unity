f8cef994098f62f3be6e2a2f9b57305de826ff89
phutaekwondo <phutaekwondo2001@gmail.com>
2023-03-22 01:57:35 +0700

    add a tooltip for config radius

--- Assets/BallsSetAssest/BallsSetConfig.cs
+++ Assets/BallsSetAssest/BallsSetConfig.cs
@@ -10,11 +10,15 @@ public class BallsSetConfig : MonoBehaviour
     }
     private float m_ballRadius = 0.032f;
     List<Vector2> m_15BallsPositionList = new List<Vector2>();
+    [Tooltip("To apply this radius, Click the \"Set Balls Radius\" button")]
     [SerializeField] float m_configBallRadius = 0.032f;
 
+    //private methods
+    //public methods
     public void SetBallsRadius()
     {
         //set the ball radius
+        m_ballRadius = m_configBallRadius;
         //calculate the positions
         //set the positions
         //set the scale
