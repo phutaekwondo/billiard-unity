55ec199504df8438d30c4d6373d00a59bda8c868
phutaekwondo <phutaekwondo2001@gmail.com>
2023-03-28 15:05:25 +0700

    set up for testing

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GeometrySlave;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private GameObject m_simpleSpherePrefab;
+    [SerializeField] private GeometrySlave m_geometrySlave;
+}
